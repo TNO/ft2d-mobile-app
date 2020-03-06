b412e825ec3e7770263fdbbbe75bb491a59e42cc
Serdar Ozsezen <serdar.ozsezen@tno.nl>
2020-03-06 18:43:06 +0100

    Added icon

--- EASMEapp/App.tsx
+++ EASMEapp/App.tsx
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { StyleSheet, 
          Text, 
          View,
-         Button,
          TextInput,
          Picker,
          KeyboardAvoidingView,
-         
+         TouchableOpacity,
+         Keyboard,
+        //  Platform
         } from 'react-native';
-import {ToastAndroid} from 'react-native';
-import * as Font from 'expo-font';
-
+import { ToastAndroid } from 'react-native';
 export default class App extends Component {
 
   state = {
@@ -19,47 +18,72 @@ export default class App extends Component {
     diabeticStatus: 'healthy'
   }
 
+  onPress = () => {
+    ToastAndroid.show("Pushed calculate", ToastAndroid.SHORT);
+  }
+
   render(){
     return(
       <KeyboardAvoidingView 
-        behavior="padding" style={{ backgroundColor: 'powderblue', flex: 1}}>
-
-            <View style={{ backgroundColor: 'white',width:'100%',height:'50%'}}/>
+        behavior="padding" 
+        style={{ backgroundColor: 'white', flex: 1, flexDirection: 'column' }}
+        keyboardVerticalOffset={-30}>
+            <View style={{ backgroundColor: 'steelblue',width:'100%',height:'50%'}}/>
             <View style={{ backgroundColor: 'pink', width: '100%',height:'10%' }}>
-              <View style={{ flex: 1 }}>
+              <View style={{ flex: 1, flexDirection: 'row'}}>
+                <View style={{ flex: 1, justifyContent:'center',backgroundColor:'white' }}>
+                  <Text style={{ fontSize: 20, textAlign:'center'}}>I am</Text>
+                </View>
+                <View style={{ flex: 1, justifyContent:'center', borderWidth:3 }}>
+                <Picker
+                  selectedValue={this.state.diabeticStatus}
+                  onValueChange={(val) => {this.setState({diabeticStatus:val})}}
+                  itemStyle={{ fontSize: 25, alignItems: 'center'}}>
+                    <Picker.Item label="healthy" value="healthy"></Picker.Item>
+                    <Picker.Item label="diabetic" value="diabetic"></Picker.Item>
+                  </Picker>
+                </View>
 
               </View>
             </View>
-            <View style={{ backgroundColor: 'yellow', width: '100%', height: '10%'}}>
+            <View style={{ backgroundColor: 'white', width: '100%', height: '10%'}}>
               <View style={{ flex: 1, justifyContent: 'center'}}>
                 <Text style={{  fontSize: 20, marginLeft: '2%' }}>
-                  Glucose level input:
+                  My blood glucose level:
                 </Text>
               </View>
             </View>
-            <View style={{ backgroundColor: 'grey', width: '100%', height: '20%'}}>
-            <View style={{ flex: 1,flexDirection: 'row' }}>
-              <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center' }}>
-                <TextInput style={{ fontSize: 35, textAlign: 'center'}} 
-                          keyboardType='decimal-pad'
-                          onChangeText={(level) => {
-                            this.setState({glucoseLevel: Number.parseFloat(level)});
-                            //  ToastAndroid.show(`Glucose level is ${this.state.glucoseLevel}`,ToastAndroid.SHORT);
-                            }}>
-                </TextInput>
+            <View style={{ backgroundColor: 'grey', width: '100%', height: '15%'}}>
+              <View style={{ flex: 1,flexDirection: 'row' }}>
+                <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', borderWidth:3, borderLeftWidth:0 }}>
+                  <TextInput style={{ fontSize: 35, textAlign: 'center'}} 
+                            keyboardType='decimal-pad'
+                            onChangeText={(level) => {
+                              this.setState({glucoseLevel: Number.parseFloat(level)});
+                              }}
+                              onEndEditing={() => Keyboard.dismiss()}>
+                  </TextInput>
+                </View>
+                <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center',
+                              borderWidth:3 ,borderLeftWidth:0, borderRightWidth:0 }}>
+                  <Picker 
+                    selectedValue={this.state.unit}
+                    onValueChange={(u) => this.setState({unit: u})}>
+                    <Picker.Item label="mmol/L" value="mmol/L" />
+                    <Picker.Item label="mg/dL" value="mg/dL" />
+                  </Picker>
+                </View>
               </View>
-              <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center' }}>
-                <Picker 
-                  
-                  selectedValue={this.state.unit}
-                  onValueChange={(u) => this.setState({unit: u})}>
-                  <Picker.Item label="mmol/L" value="mmol/L" />
-                  <Picker.Item label="mg/dL" value="mg/dL" />
-                </Picker>
+          </View>
+            <View style={{ backgroundColor: 'pink', width:'100%', height: '15%' }}>
+              <View style={{ flex: 1, justifyContent:'center'}}>
+                <TouchableOpacity
+                  onPress={this.onPress}>
+                  <Text style={{ fontSize: 40, textAlign: 'center'}}>Calculate</Text>
+                </TouchableOpacity>
+                
               </View>
             </View>
-          </View>
-
       </KeyboardAvoidingView>
     )
   }
